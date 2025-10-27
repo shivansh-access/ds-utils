22f4354496dddfb8723a43615e0a39d52b71c2b1
shivansh-access <shivanshaccess@gmail.com>
2025-10-27 16:21:48 +0530

    Added function to print first element of an array

--- arrays/basic-array-operations.py
+++ arrays/basic-array-operations.py
@@ -6,8 +6,15 @@ def print_array_elements(input_arr):
         print(f"Printing index: {i}")
 
     for i in range(0, len(input_arr)):     
-       print(f"Printing elements: {input_arr[i]}")
+       print(f"Printing element: {input_arr[i]}")
 
 
-# print_array_elements(["I", "am", "ready"])
-# print_array_elements([11, 32, 42, 90, 22, 10])  
+def print_first_element_of_the_array(input_arr):
+   print(f"printing the first element: {input_arr[3]}")
+
+print(print_first_element_of_the_array("5" "10"))
+    
+
+
+#print_array_elements(["I", "am", "ready"])
+# print_array_elements([11, 32, 42, 90, 22, 10]) 
